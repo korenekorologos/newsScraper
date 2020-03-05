5a87d86ebfbcb7101901194d87169f1342b7d380
korene korologos <korenekorologos@gmail.com>
2020-03-05 09:44:13 -0700

    some routers have been added to the controller.js file

--- controller/controller.js
+++ controller/controller.js
@@ -20,20 +20,70 @@ router.get("/", function (req, res) {
 
 //get request to scrape the website that we want to "get"
 router.get("/scrape", function (req, res) {
-    request("https://theweek.com/", function (error, response, html) {
+    requestPromise("https://theweek.com/", function (error, response, html) {
         //this will load the info into cheerio and save it into the selector 
         var $ = cheerio.load(html);
         var titlesArray = [];
-        
+
         //this will grab every class tage from the site that we're pulling from 
-        
+        $(".c-entry-box--compact_title").each(function (i, element) {
+            var result{ };
 
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
 
-    })
+            //this will check for empty or duplicates before pushing into the database
+            //function for the end point scrape 
+            if (result.title !== "" && result.link !== "") {
+                if (titlesArray.indexOf(result.title) == -1) {
+                    titlesArray.push(result.title);
+
+                    article.count({ title: result.title }, function (err, test) {
+                        if (test === 0) {
+                            var entry = new article(result);
+
+                            entry.save(function (err, doc) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    console.log(doc);
+                                }
+                            });
+                        }
+                    });
+                } else {
+                    console.log("Sorry, butthis article already exitsis");
+                } else {
+                    console.log("This is missing some data");
+                }
+            });
+        res.redirect("/");
+    });
 });
 
+router.get("/articles", function (req, res) {
+    article.find()
+        .sort({ _id: -1 })
+        .exec(function (err, doc) {
+            if (err) {
+                console.log(err);
+            } else {
+                var artcl = { article: doc };
+                res.render("index", artcl);
+            }
+        });
+});
 
-
-
-
-
+router.get("/articles-json", function (req, res) {
+    article.find{ }, function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(doc);
+        }
+    }); 
+}); 
